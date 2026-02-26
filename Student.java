4a54a79fe55dea77d99003624bb27ac36e3ba4b9
shafiabajwaa <FA25-BSE-114@cuilahore.edu.pk>
2026-02-26 10:11:53 +0500

    basic java program

--- Student.java
+++ Student.java
@@ -1,63 +1,27 @@
 public class Student {
 
-    private String name;
-    private int rollNo;
-    private double marks;
+    int id;
+    String studentName;
+    String program;
 
-    private static String universityName = "Comsats University";
-    private static int studentCount = 0;
-
-    public final double MAX_MARKS = 100;
-
-    public Student(String name, int rollNo, double marks) {
-        this.name = name;
-        this.rollNo = rollNo;
-        this.marks = marks;
-        studentCount++;
+    void showDetails() {
+        System.out.println("Student ID: " + id);
+        System.out.println("Name: " + studentName);
+        System.out.println("Program: " + program);
     }
 
-    public void displayStudentInfo() {
-        System.out.println("Name: " + name);
-        System.out.println("Roll No: " + rollNo);
-        System.out.println("Marks: " + marks);
-        calculateGrade();
-        System.out.println();
-    }
-
-    public void calculateGrade() {
-        if (marks >= 80) {
-            System.out.println("Grade: A");
-        } else if (marks >= 60) {
-            System.out.println("Grade: B");
-        } else if (marks >= 50) {
-            System.out.println("Grade: C");
-        } else {
-            System.out.println("Grade: Fail");
-        }
-    }
-
-    public static void displayUniversity() {
-        System.out.println("University: " + universityName);
-        System.out.println("Total Students: " + studentCount);
-        System.out.println();
-    }
-
-    public final void showMaxMarks() {
-        System.out.println("Maximum Marks: " + MAX_MARKS);
+    void attendClass() {
+        System.out.println("Student is attending: " + program);
     }
 
     public static void main(String[] args) {
 
-        Student s1 = new Student("Ali", 101, 85);
-        Student s2 = new Student("Sara", 102, 67);
+        Student obj = new Student();
+        obj.id = 201;
+        obj.studentName = "Sara";
+        obj.program = "CS";
 
-        s1.displayStudentInfo();
-        s2.displayStudentInfo();
-
-        s1.showMaxMarks();
-
-        displayUniversity();
-
-       
+        obj.showDetails();
+        obj.attendClass();
     }
 }
